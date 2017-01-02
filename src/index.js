668aae73d96c1481fae8db0ab2d299bc77a542f3
Devon Bradley <dbradleyfl@gmail.com>
2017-01-02 04:26:21 -0500

    implement autolaunch

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@
 const path = require('path')
 const menubar = require('menubar')
 const { ipcMain } = require('electron')
+const AutoLaunch = require('auto-launch');
 
 var mb = menubar({
   'index': path.join('file://', __dirname, '/index.html')
 });
+
+var appAutoLauncher = new AutoLaunch({
+    name: 'Cali Surf Check Menubar',
+    path: __dirname,
+});
+
+appAutoLauncher.enable();
 
 mb.on('show', function () {
   mb.window.webContents.send('reload-report', 'reload');
